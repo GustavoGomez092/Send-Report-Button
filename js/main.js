206caa6e455bec9d33c13889f18bc6db07e304a3
gustavo <ggo@topfloormarketing.net>
2019-03-18 15:54:53 -0600

    latest style changes to prevent multiple button click

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 document.getElementById('executeSend').addEventListener('click',
 
     function loadXMLDoc() {
+        document.getElementById('loader').classList.remove('hidden');
+        this.disabled = true;
+        this.classList.add('disabled');
         const Http = new XMLHttpRequest();
         const url = 'http://localhost:5000/sendEmails';
         Http.open("POST", url);
         Http.send();
         Http.onreadystatechange = (e) => {
-            console.log(Http.responseText)
+          document.getElementById('loader').classList.add('hidden');
+          document.getElementById('demo').innerHTML = Http.responseText;
+          document.getElementById('executeSend').disabled = false;
+          this.classList.remove('disabled');
         }
     }
     
